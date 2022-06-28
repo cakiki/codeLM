3d7b672c8310b498853036322bf65b314ca4e6ee
Christopher Akiki <christopher.akiki@protonmail.com>
2022-06-28 22:46:39 +0200

    Update spider

--- code_datasets/code_datasets/spiders/rosetta_code.py
+++ code_datasets/code_datasets/spiders/rosetta_code.py
@@ -16,7 +16,13 @@ class RosettaSpider(scrapy.Spider):
         yield from response.follow_all(tasks, callback=self.parse_task)
     
     def parse_task(self, response):
-        
-        yield {
-            
-        }
+        toc_links = toc_links = response.xpath('//div[@id="toc"]/ul/li/a/@href').getall()
+        toc_languages = response.xpath('//div[@id="toc"]/ul/li/a/span[@class="toctext"]').getall()
+        assert len(toc_links) == len(toc_languages)
+        for link, language in zip(toc_links, toc_languages):
+            yield {
+                "task_url": response.url,
+                "language_url": link,
+                "language_name": bs_strip_tags(language),
+                "code": bs_strip_tags(response.xpath(f'//h2[span[@id="{link[1:]}"]]/following-sibling::pre').get().replace('<br>', '\n')),
+            }
