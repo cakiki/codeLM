9da24f56af409cedddddc6f4dd9dfc549349b45d
Christopher Akiki <christopher.akiki@protonmail.com>
2022-07-01 09:02:48 +0200

    Add metadata spider

--- code_datasets/code_datasets/spiders/rosetta_code.py
+++ code_datasets/code_datasets/spiders/rosetta_code.py
@@ -3,13 +3,15 @@ from bs4 import BeautifulSoup
 
 def bs_strip_tags(html):
     soup = BeautifulSoup(html, 'html.parser')
+    a = soup.find_all('a', {'class':'__cf_email__'})
+    for i in a:
+        i.string = decode_email(i.attrs['data-cfemail'])  
     return soup.get_text()
 
 def decode_email(email):
     decoded = ""
-    for i in range(2, len(e)-1, 2):
-        decoded += chr(int(e[i:i+2], 16)^(int(email[:2], 16)))
-
+    for i in range(2, len(email)-1, 2):
+        decoded += chr(int(email[i:i+2], 16)^(int(email[:2], 16)))
     return decoded
 
 class RosettaSpider(scrapy.Spider):
@@ -27,18 +29,9 @@ class RosettaSpider(scrapy.Spider):
         toc_languages = response.xpath('//div[@id="toc"]/ul/li/a/span[@class="toctext"]').getall()
         assert len(toc_links) == len(toc_languages)
         for link, language in zip(toc_links, toc_languages):
-            pre = response.xpath(f'//h2[span[@id="{toc_links[0][1:]}"]]/following-sibling::pre').get()
-            soup = BeautifulSoup(pre, 'html.parser')
-            a = soup.find('a')
-            if a is None:
-                code = bs_strip_tags(response.xpath(f'//h2[span[@id="{link[1:]}"]]/following-sibling::pre').get().replace('<br>', '\n'))
-            elif 'data-cfemail' in a.attrs:
-                code = decode_email(a.attrs['data-cfemail'])
-            else:
-                code = "<EMPTY>"
             yield {
                 "task_url": response.url,
                 "language_url": link,
                 "language_name": bs_strip_tags(language),
-                "code": code,
+                "code": bs_strip_tags(response.xpath(f'//h2[span[@id="{link[1:]}"]]/following-sibling::pre').get().replace('<br>', '\n')),
             }
